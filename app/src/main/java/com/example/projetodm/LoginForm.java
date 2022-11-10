63b600f3816fa27842c9a345afdf7e647eeb514f
Gustavo-Zamai <gustavosimaozamai@gmail.com>
2022-11-10 20:24:29 -0300

    Add some stuffs into first screen view

--- app/src/main/java/com/example/projetodm/LoginForm.java
+++ app/src/main/java/com/example/projetodm/LoginForm.java
@@ -10,5 +10,7 @@ public class LoginForm extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_form);
+        getSupportActionBar().hide();
+
     }
 }
