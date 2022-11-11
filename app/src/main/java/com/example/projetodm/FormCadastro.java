f29f57ee12d2df3fb51235c8033b4b3fc36f22e4
Gustavo-Zamai <gustavosimaozamai@gmail.com>
2022-11-11 15:52:57 -0300

    Add second screen into project

--- app/src/main/java/com/example/projetodm/FormCadastro.java
+++ app/src/main/java/com/example/projetodm/FormCadastro.java
@@ -10,5 +10,10 @@ public class FormCadastro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_cadastro);
+        getSupportActionBar().hide();
+
+
+
     }
+
 }
--- app/src/main/java/com/example/projetodm/LoginForm.java
+++ app/src/main/java/com/example/projetodm/LoginForm.java
@@ -2,15 +2,35 @@ package com.example.projetodm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginForm extends AppCompatActivity {
+
+    private TextView text_tela_cadastro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_form);
         getSupportActionBar().hide();
+        IniciarComponentes();
+
+        text_tela_cadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(LoginForm.this, FormCadastro.class);
+                startActivity(i);
+            }
+        });
 
     }
+
+    private void IniciarComponentes(){
+        text_tela_cadastro = findViewById(R.id.text_tela_cadastro);
+    }
+
 }
